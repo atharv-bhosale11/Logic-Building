3ed39036582fb727a980174aa881ea2809f23972
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 20:04:52 +0530

    Create Assignment 6_Q4.c

---
+++ Assignment 6/Assignment 6_Q4.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+void Table(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+      iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= 10; iCnt++)
+    {
+        printf("%d\n",iNo * iCnt);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+    
+    Table(iValue);
+
+    return 0;
+}
+
+// Time Complexity is O(1)
