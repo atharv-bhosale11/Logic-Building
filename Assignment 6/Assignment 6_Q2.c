a2907e094b21d6bd70c2d6d7d21cfe55553c6c90
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 19:56:07 +0530

    Create Assignment 6_Q2.c

---
+++ Assignment 6/Assignment 6_Q2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+void Display(int iNo)
+{   
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+    
+    if(iNo == 1)
+    {
+        printf("One");
+    }
+    else if(iNo == 2)
+    {
+        printf("Two");
+    }
+    else if(iNo == 3)
+    {
+        printf("Three");
+    }
+    else if(iNo == 4)
+    {
+        printf("Four");
+    }
+    else if(iNo == 5)
+    {
+        printf("Five");
+    }
+    else
+    {
+        printf("Invalid Number");
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+    
+    Display(iValue);
+
+    return 0;
+}
+
+// Time Complexity is O(1)
