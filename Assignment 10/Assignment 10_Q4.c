4f26b858e04d4a13f7d17c26f689b51f32e1c052
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 22:20:07 +0530

    Create Assignment 10_Q4.c

---
+++ Assignment 10/Assignment 10_Q4.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+double FhtoCs(float fTemp)
+{
+    float fCelsius = 0.0;
+
+    fCelsius = (fTemp - 32) * 5 / 9;
+
+    return fCelsius;
+
+}
+
+int main()
+{
+    float fValue = 0.0;
+    double dRet = 0.0;
+    
+    printf("Enter Temperature in Fahreneheit: ");
+    scanf("%f",&fValue);
+
+    dRet = FhtoCs(fValue);
+
+    printf("Celsius Value is: %lf",dRet);
+
+    return 0;
+}
+
+// Time Complxity is O(1)
