105da9281cac8d558dd88e8a7733c3e8da7bb587
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 21:58:19 +0530

    Create Assignment 9_Q3.c

---
+++ Assignment 9/Assignment 9_Q3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int CountRange(int iNo)
+{
+    int iCnt = 0;
+    int iDigit = 0;
+
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+
+        if(iDigit >= 3 && iDigit <=7)
+        {
+            iCnt++;
+        }
+        iNo = iNo / 10;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+
+    iRet = CountRange(iValue);
+
+    printf("Digits in between 3 to 7: %d",iRet);
+    return 0;
+}
+
+// Time Complexity is O(log n)
