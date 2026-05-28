e202d7be90cd4182da5e4dd0a18965c62b1db3da
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 19:27:04 +0530

    Create Assignment 5_Q1.c

---
+++ Assignment 5/Assignment 5_Q1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int Pattern(int iNo)
+{
+    int iCnt = 0;
+
+    if(iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    for(iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("$ *\t",iCnt);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+    
+    Pattern(iValue);
+
+    return 0;
+}
