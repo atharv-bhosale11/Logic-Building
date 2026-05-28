36ceaa7cceece57c3c77015290f7ee5b2eea968a
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 22:19:48 +0530

    Create Assignmeng 10_Q3.c

---
+++ Assignment 10/Assignmeng 10_Q3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int KMtoMeter(int iNo)
+{
+    int iMet = 0;
+
+    iMet = iNo * 1000;
+
+    return iMet;
+}
+
+int main()
+{
+    int iValue = 0, iRet = 0;
+    
+    printf("Enter Distance: ");
+    scanf("%d",&iValue);
+
+    iRet = KMtoMeter(iValue);
+
+    printf("Converted Value is: %d",iRet);
+
+    return 0;
+}
+
+// Time Complexity is O(1)
