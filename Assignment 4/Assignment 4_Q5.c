158749625c37de1dbdb7049b7f0b4ff6c60347d6
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 19:12:36 +0530

    Create Assignment 4_Q5.c

---
+++ Assignment 4/Assignment 4_Q5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int FactDiff(int iNo)
+{
+    int iCnt = 0;
+    int iSumNonFact = 0;
+    int iSumFact = 0;
+    int iDiff = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if(iNo % iCnt == 0)
+        {
+            iSumFact = iSumFact + iCnt;
+        }
+        else
+        {
+            iSumNonFact = iSumNonFact + iCnt;
+        }
+    }
+
+    iDiff = iSumFact - iSumNonFact;
+
+    return iDiff;
+}
+
+int main()
+{
+    int iValue = 0;
+    int bRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+    
+    bRet = FactDiff(iValue);
+
+    printf("Difference is:  %d",bRet);
+
+    return 0;
+}
