d8cb242b52423b18bb0792b9aa80fe66b5a88a21
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 18:57:49 +0530

    Create Assignment 4_Q2.c

---
+++ Assignment 4/Assignment 4_Q2.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int FactREv(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = iNo/2; iCnt >= 1; iCnt--)
+    {
+        if(iNo % iCnt == 0)
+        {
+            printf("%d\t",iCnt);
+        }
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+    
+    FactREv(iValue);
+
+    return 0;
+}
