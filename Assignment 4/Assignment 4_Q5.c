f700ca7432f22147bdb9ac76e6aab2e8aac6950a
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 19:20:57 +0530

    Update Assignment 4_Q5.c

--- Assignment 4/Assignment 4_Q5.c
+++ Assignment 4/Assignment 4_Q5.c
@@ -38,3 +38,86 @@ int main()
 
     return 0;
 }
+
+#include<stdio.h>
+
+int FactDiff(int iNo)
+{
+    int iCnt = 0;
+    int iSumNonFact = 0;
+    int iSumFact = 0;
+    int iDiff = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if(iNo % iCnt == 0)
+        {
+            iSumFact = iSumFact + iCnt;
+        }
+        else
+        {
+            iSumNonFact = iSumNonFact + iCnt;
+        }
+    }
+
+    iDiff = iSumFact - iSumNonFact;
+
+    return iDiff;
+}
+
+int main()
+{
+    int iValue = 0;
+    int bRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+    
+    bRet = FactDiff(iValue);
+
+    printf("Difference is:  %d",bRet);
+
+    return 0;
+}
+#include<stdio.h>
+
+int FactDiff(int iNo)
+{
+    int iCnt = 0;
+    int iSumNonFact = 0;
+    int iSumFact = 0;
+    int iDiff = 0;
+
+    for(iCnt = 1; iCnt < iNo; iCnt++)
+    {
+        if(iNo % iCnt == 0)
+        {
+            iSumFact = iSumFact + iCnt;
+        }
+        else
+        {
+            iSumNonFact = iSumNonFact + iCnt;
+        }
+    }
+
+    iDiff = iSumFact - iSumNonFact;
+
+    return iDiff;
+}
+
+int main()
+{
+    int iValue = 0;
+    int bRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+    
+    bRet = FactDiff(iValue);
+
+    printf("Difference is:  %d",bRet);
+
+    return 0;
+}
+
+//Time Complexity is 0(n)
