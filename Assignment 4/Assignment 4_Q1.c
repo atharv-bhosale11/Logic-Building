0a1b6696f02ce17e60b57c12a66926ebec9fb615
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 18:51:50 +0530

    Create Assignment 4_Q1.c

---
+++ Assignment 4/Assignment 4_Q1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int MulFact(int iNo)
+{
+    int iCnt = 0;
+    int iMul = 1; 
+
+    for(iCnt = 1; iCnt <= iNo/2; iCnt++)
+    {
+        if(iNo % iCnt == 0)
+        {
+            iMul = iMul * iCnt;
+        }
+    }
+    
+    return iMul;
+}
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+    
+    iRet = MulFact(iValue);
+
+    printf("Multiplication of Factor is %d",iRet);
+
+    return 0;
+}
