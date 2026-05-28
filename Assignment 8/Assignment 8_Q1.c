3c4ffe815327f22a9428009dc46b1cf3887fe14f
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-28 20:57:54 +0530

    Create Assignment 8_Q1.c

---
+++ Assignment 8/Assignment 8_Q1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+double CircleArea(float fRadius)
+{
+    float fPi = 3.14;
+    float fArea = 0.0;
+
+    fArea = fPi * fRadius * fRadius;
+
+    return fArea;
+}
+
+int main()
+{
+    float fValue = 0.0;
+    double dRet = 0;
+
+    printf("Enter Radius: ");
+    scanf("%f",&fValue);
+
+    dRet = CircleArea(fValue);
+
+    printf("Area of Circle is: %lf ",dRet);
+
+    return 0;
+}
+
+// Time Complexity is O(1)
