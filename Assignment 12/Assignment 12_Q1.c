715478242bf9578aab6ffe0567abffe37be01a6f
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-05-30 17:21:41 +0530

    Create Assignment 12_Q1.c

---
+++ Assignment 12/Assignment 12_Q1.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void DisplayDigit(int iNo)
+{
+    int iCnt = 0, iDigit = 0;
+
+    if (iNo < 0)
+    {
+        iNo = -iNo;
+    }
+
+    while (iNo != 0)
+    {
+        iDigit = iNo % 10;
+        printf("%d",iDigit);
+        iNo = iNo / 10;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+
+    DisplayDigit(iValue);
+
+   return 0;
+}
+
+// Time Complexity is O(d)
