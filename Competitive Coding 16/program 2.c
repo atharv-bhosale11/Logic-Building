033ba12db86f6ae80c1337ebed0d63db3b867016
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-04 12:06:41 +0530

    Create program 2.c

---
+++ Competitive Coding 16/program 2.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+int Minimum(int Arr[],int iLength)
+{
+    int iCnt = 0, iMin = Arr[0];
+
+    for(iCnt = 1; iCnt < iLength; iCnt++)
+    {
+        if(Arr[iCnt] < iMin)
+        {
+            iMin = Arr[iCnt];
+        }
+    }
+    return iMin;
+}   
+
+int main()
+{
+    int iSize = 0,iCnt = 0, iRet = 0;
+    int *p = NULL;
+
+    printf("Enter the Size of Array: ");
+    scanf("%d",&iSize);
+
+    p = (int *)malloc(iSize*sizeof(int));
+
+    if(p == NULL)
+    {
+        printf("Unable to allocate the Memory");
+        return -1;
+    }
+
+    printf("Enter the Elements : ");
+
+    for (iCnt = 0 ; iCnt < iSize ; iCnt++)
+    {
+        scanf("%d",&p[iCnt]);
+    }
+
+    iRet = Minimum(p,iSize);
+
+    printf("Minimum Number is %d",iRet);
+    
+    free(p);
+
+    return 0;
+}
