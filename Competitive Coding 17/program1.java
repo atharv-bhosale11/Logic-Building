1d387dd6f6545a73ebaa1cb3552b6dd8c690088a
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-08 20:10:38 +0530

    Create program1.java

---
+++ Competitive Coding 17/program1.java
@@ -0,0 +1,24 @@
+class Logic
+{
+    void sumofDigits(int num)
+    {
+        int iCnt = 0, iSum = 0, iDigit = 0;
+
+        while(num != 0)
+        {
+            iDigit = num % 10;
+            iSum = iSum + iDigit;
+            num = num / 10;
+        }
+        System.out.println("Sum of Digits = "+iSum);
+    }
+}
+
+class program1
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.sumofDigits(1234);
+    }
+}
