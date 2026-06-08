8ce40ab1c6fe71e475fcd56fbddd9e1da42fd722
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-08 20:16:46 +0530

    Create program2.java

---
+++ Competitive Coding 17/program2.java
@@ -0,0 +1,34 @@
+class Logic
+{
+    void checkPalindrome(int num)
+    {
+        int iCnt = 0, iDigit = 0, iRev = 0;
+        int temp = num;
+
+        while(num != 0)
+        {
+            iDigit = num % 10;
+            iRev = (iRev * 10) + iDigit;
+            num = num / 10;
+        }
+
+        if (iRev == temp)
+        {
+            System.out.println("Number is Palindrome");
+        }
+        else
+        {
+            System.out.println("Number is not palindrome");
+        }
+
+    }
+}
+
+class program2
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.checkPalindrome(121);
+    }
+}
