ab6fc2937acb4408a9a21c95aaf8a0f05c44ecdc
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-08 20:40:02 +0530

    Create program5.java

---
+++ Competitive Coding 17/program5.java
@@ -0,0 +1,21 @@
+class Logic
+{
+    void printTable(int num)
+    {
+       int iCnt = 0;
+
+       for(iCnt = 1; iCnt <= 10; iCnt++)
+       {
+        System.out.println(num+" * "+iCnt+" = "+num*iCnt);
+       }
+    }
+}
+
+class program5
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.printTable(5);
+    }
+}
