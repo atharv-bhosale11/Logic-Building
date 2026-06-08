e956e5e1a8180e2db7ae9a6b006065ad2449f08a
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-08 20:35:20 +0530

    Create program4.java

---
+++ Competitive Coding 17/program4.java
@@ -0,0 +1,32 @@
+
+
+class Logic
+{
+    void findmin(int a, int b, int c)
+    {
+       int iMin = 0;
+
+       if(a < b && a < c)
+       {
+        iMin = a;
+       }
+       else if(b < a && b < c )
+       {
+        iMin = b;
+       }
+       else
+       {
+        iMin = c;
+       }
+       System.out.println(iMin+ " is lesser");
+    }
+}
+
+class program4
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.findmin(3,7,2);
+    }
+}
