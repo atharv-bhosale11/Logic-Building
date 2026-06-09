cfc59a0996e5847b93a684d88226a2ca59ce70b1
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-09 19:59:23 +0530

    Create program1.java

---
+++ Competitive Coding 19/program1.java
@@ -0,0 +1,23 @@
+class Logic
+{
+    void CheckLeapYear(int year)
+    {
+        if((year % 4 == 0) && (year != 100) && (year % 100 != 0))
+        {
+            System.out.println("It's a Leap Year");
+        }
+        else
+        {
+            System.out.println("Its not a leap year");
+        }
+    }
+}       
+
+class program1
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.CheckLeapYear(2024);
+    }
+}
