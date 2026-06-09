0969aa2bac50832e94b517ab5414b7801ed2c9eb
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-09 20:05:25 +0530

    Create program2.java

---
+++ Competitive Coding 19/program2.java
@@ -0,0 +1,35 @@
+class Logic
+{
+    void DisplayGrade(int marks)
+    {
+        if(marks >= 90 && marks <= 100)
+        {
+            System.out.println("Grade A");
+        }
+        else if(marks >= 80)
+        {
+            System.out.println("Grade B");
+        }
+        else if(marks >= 60)
+        {
+            System.out.println("Grade C");
+        }
+        else if(marks >= 35)
+        {
+            System.out.println("Grade D");
+        }
+        else
+        {
+            System.out.println("Fail :/");
+        }
+    }
+}       
+
+class program2
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.DisplayGrade(82);
+    }
+}
