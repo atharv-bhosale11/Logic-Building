3a3c311e8969b448ee12a6a9e55968d20dadeb4d
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-09 19:49:13 +0530

    Create program4.java

---
+++ Competitive Coding 18/program4.java
@@ -0,0 +1,35 @@
+class Logic
+{
+    void sumEvenOddDigits(int num)
+    {
+        int iDigit = 0, iSumEven = 0, iSumOdd = 0;
+
+        while(num != 0)
+        {
+            iDigit = num % 10;
+
+            if(iDigit % 2 == 0)
+            {
+                iSumEven = iSumEven + iDigit;
+            }
+            else
+            {
+                iSumOdd = iSumOdd + iDigit;
+            }
+            num = num / 10;
+        }
+        System.out.println("Even Sum = "+iSumEven);
+        System.out.println("Odd Sum = "+iSumOdd);
+    }
+      
+}       
+
+class program4
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.sumEvenOddDigits(123456);
+        
+    }
+}
