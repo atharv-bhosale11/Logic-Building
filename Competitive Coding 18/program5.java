d203f331e4324e6d6c1045ff7ca2429f704b0cc8
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-09 19:52:08 +0530

    Create program5.java

---
+++ Competitive Coding 18/program5.java
@@ -0,0 +1,28 @@
+class Logic
+{
+    void checkSign(int num)
+    {
+        if(num > 0)
+        {
+            System.out.println("Number is Positive");
+        }
+        else if(num < 0)
+        {
+            System.out.println("Number is Negative");
+        }
+        else
+        {
+            System.out.println("Its Zero");
+        }
+    }
+}       
+
+class program5
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.checkSign(-8);
+        
+    }
+}
