64938a95ee5147db63a3f1f101443d97df854374
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-10 09:34:31 +0530

    add program to check perfect numbers (Type 1)  - Implemented CheckPerfet method in Logic class - Iterates through divisors of input number - Accumulates sum of proper divisors (excluding the number itself)

---
+++ Competitive Coding 20/program3.java
@@ -0,0 +1,32 @@
+class Logic
+{
+    void CheckPerfet(int num)
+    {
+        int iCnt = 0, iSum = 0;
+
+        for(iCnt = 1; iCnt < num; iCnt++)
+        {
+            if(num % iCnt == 0)
+            {
+                iSum = iSum + iCnt;
+            }
+        }
+        if(iSum == num)
+        {
+            System.out.println("Number is Perfect");
+        }
+        else
+        {
+            System.out.println("Number is not Perfect");
+        }
+    }
+}
+
+class program3
+{
+    public static void main(String A[])
+    {
+        Logic lobj = new Logic();
+        lobj.CheckPerfet(6);;
+    }
+}
