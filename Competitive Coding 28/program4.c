06ce9fe90543b5b3a7daf2354110c30f45413801
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-17 18:01:56 +0530

    add C program to demonstrate star, hash, and dollar pattern printing with reverse diagonal and border detection  - created `Pattern(int iRow, int iCol)` function to print rectangular pattern - used nested `for` loops to iterate through rows and columns - applied condition `if(j == (iCol - i) + 1 || i == 1 || i == iRow || j == 1 || j == iCol)` to print `*` on reverse diagonal and border positions - printed `$` when column index is greater than reverse diagonal position (right side region) - printed `#` when column index is less than reverse diagonal position (left side region) - accepted user input for number of rows and columns using `scanf` - invoked `Pattern(iValue1, iValue2)` to display pattern based on user input - reinforced understanding of nested loops, conditional logic, reverse diagonal detection, border detection, and multi-symbol pattern generation in C

---
+++ Competitive Coding 28/program4.c
@@ -0,0 +1,51 @@
+/*
+
+Enter the No. of rows and columns: 6
+6
+*       *       *       *       *       *
+*       #       #       #       *       *
+*       #       #       *       $       *
+*       #       *       $       $       *
+*       *       $       $       $       *
+*       *       *       *       *       *
+
+*/
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if(j==(iCol - i) + 1 || i == 1 || i == iRow || j == 1 || j == iCol)
+            {
+                printf("*\t");
+            }
+            else if(j > (iCol - i)+ 1)
+            {
+                printf("$\t");
+            }
+            else
+            {
+                printf("#\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the No. of rows and columns: ");
+    scanf("%d%d",&iValue1,&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+}
