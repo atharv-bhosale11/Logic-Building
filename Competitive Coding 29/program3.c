2931f4ec2636355a284fec162f28cd8407bdc14a
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-23 11:51:54 +0530

    Add C program to check whether a given character is a digit using a separate `ChkDigit` function.   Implemented validation with conditional checks for numeric range (`'0'–'9'`), returning `TRUE` if the input is a digit and `FALSE` otherwise, with appropriate output messages in `main`.

---
+++ Competitive Coding 29/program3.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+
+#define TRUE 1
+#define FALSE 0
+
+typedef int BOOL;
+
+BOOL ChkDigit(char ch)
+{
+    if(ch >= '0' && ch <= '9')
+    {
+        return TRUE;
+    }
+    return FALSE;
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter the Character: ");
+    scanf("%c",&cValue);
+
+    bRet = ChkDigit(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is Digit");
+    }
+    else
+    {
+        printf("It is not a Digit");
+    }
+    return 0;
+}
