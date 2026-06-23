f61c05eaea518a206ccccfc7c82627721f46cacc
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-23 13:22:24 +0530

    Add C program to reverse a string using two-pointer technique   Implemented Reverse function with START and END pointers swapping characters until they meet, effectively reversing the string in place.   Program accepts user input string and prints the reversed result.

---
+++ Competitive Coding 31/program5.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void Reverse(char *str)
+{
+    char *START = NULL;
+    char *END = NULL;
+    char temp = '\0';
+
+    START = str;
+    
+    while(*str != '\0')
+    {
+        str++;
+    }
+
+    str--;
+    END = str;
+
+    while(START < END)
+    {
+        temp = *START;
+        *START = *END;
+        *END = temp;
+        START++;
+        END--;
+    }
+}
+
+int main()
+{
+    char arr[20];
+
+    printf("Enter String:  ");
+    scanf("%[^'\n']s",arr);
+
+    Reverse(arr);
+    printf(arr);
+    return 0;
+}
