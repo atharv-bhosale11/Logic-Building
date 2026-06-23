3ad876777dec4f0d25352870d9e68f4b96014945
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-23 12:57:08 +0530

    Add C program to calculate difference between count of small and capital letters in a string   Implemented Difference function to iterate through characters, incrementing counters for lowercase and uppercase letters, then returning their difference.   Program accepts user input string and prints the computed difference between small and capital letters.

--- Competitive Coding 31/program3.c
+++ Competitive Coding 31/program3.c
@@ -1,34 +1,33 @@
-#include<stdio.h>
+    #include<stdio.h>
 
-int Difference(char *str)
-{   
-    int iCntSmall = 0, iCntCapital = 0, iDiff = 0;
-    while(*str != '\0')
-    {
-        if(*str >= 'a' && *str <= 'z')
+    int Difference(char *str)
+    {   
+        int iCntSmall = 0, iCntCapital = 0;
+        while(*str != '\0')
         {
-            iCntSmall++;
+            if(*str >= 'a' && *str <= 'z')
+            {
+                iCntSmall++;
+            }
+            else
+            {
+                iCntCapital++;
+            }
+            *str++;
         }
-        else
-        {
-            iCntCapital++;
-        }
-        *str++;
+        return iCntSmall - iCntCapital;
     }
-    iDiff = iCntSmall - iCntCapital;
-    return iDiff;
-}
 
-int main()
-{
-    char arr[20];
-    int iRet = 0;
+    int main()
+    {
+        char arr[20];
+        int iRet = 0;
 
-    printf("Enter String: ");
-    scanf("%[^'\n]s",arr);
+        printf("Enter String: ");
+        scanf("%[^'\n]s",arr);
 
-    iRet = Difference(arr);
-    printf("Difference is: %d",iRet);
+        iRet = Difference(arr);
+        printf("Difference is: %d",iRet);
 
-    return 0;
-}
+        return 0;
+    }
