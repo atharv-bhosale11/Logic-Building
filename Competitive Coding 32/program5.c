a311ed586460451d1c8aa5972b86396f66725e37
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-25 19:16:32 +0530

    Add C program to count white spaces in a string using custom CountWhite function   Implemented CountWhite function to iterate through characters and increment counter when a space character is found.   Program accepts user input string, calls the function, and prints the total count of white spaces as output.

---
+++ Competitive Coding 32/program5.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int CountWhite(char *str)
+{   
+    int iCnt = 0;
+    while (*str != '\0')
+    {
+        if(*str == ' ')
+        {
+            iCnt++;
+        }
+        str++;
+    }
+    return iCnt;
+}
+
+int main()
+{
+    char arr[20];
+    int iRet = 0;
+
+    printf("Enter String: ");
+    scanf("%[^\n]s",arr);
+
+    iRet = CountWhite(arr);
+
+    printf("White spaces are: %d",iRet);
+
+    return 0;
+}
