ac2668c11d4bf3d06062c9de030c5e0256c143f1
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-30 14:38:06 +0530

    Add C program to toggle 7th and 10th bits of given number   Defined `OffBit` function with mask `0x240` and applied XOR operator to flip both bits unconditionally.   Accepted user input, invoked function, and printed updated number after modification.

---
+++ Competitive Coding 36/program4.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+UINT OffBit(UINT iNo)
+{
+    UINT iMask = 0x240;
+
+    iNo = iNo ^ iMask;
+   
+    return iNo;
+}
+
+int main()
+{
+    UINT iValue = 0;
+    UINT iRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+
+    iRet = OffBit(iValue);
+
+    printf("Updated Number: %d",iRet);
+
+    return 0;
+}
