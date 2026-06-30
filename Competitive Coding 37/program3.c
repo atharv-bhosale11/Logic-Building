31fa8159fd84627e427be54dac703d23bfef421e
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-06-30 15:40:08 +0530

    Add C program to turn on specific bit of given number   Defined `OnBit` function with dynamic mask based on user-provided position.   Accepted user input for number and bit location, set bit using OR operator, and printed updated number.

---
+++ Competitive Coding 37/program3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+typedef unsigned int UINT;
+
+UINT OnBit(UINT iNo, UINT iPos)
+{
+    UINT iMask = 0x1;
+    
+    iMask = iMask << (iPos - 1);
+
+    iNo = iNo | iMask;
+
+    return iNo;
+}
+
+int main()
+{
+    UINT iValue = 0, iLocation = 0, iRet = 0;
+
+    printf("Enter Number: ");
+    scanf("%d",&iValue);
+
+    printf("Enter Location: ");
+    scanf("%d",&iLocation);
+
+    iRet = OnBit(iValue,iLocation);
+
+    printf("Updated Number : %d",iRet);
+
+    return 0;
+}
