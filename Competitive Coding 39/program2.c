a342bf1e5881bb1a1577a84261f83665d1a55d79
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-09 10:07:18 +0530

    Add C program to count even elements in singly linked list   Defined NODE structure and typedefs for node pointers.   Implemented InsertFirst to add nodes at the beginning of the list.   Added Display function to traverse and print all nodes.   Created CountEven function to calculate number of even elements.   Integrated user input in main to build list, display elements, and show count of even numbers.

---
+++ Competitive Coding 39/program2.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int No)
+{
+    PNODE newn = NULL;
+    
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = No;
+    newn -> next = NULL;
+
+    if(NULL == *first)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("|%d| -> ",first -> data);
+        first = first -> next;
+    }
+    printf("NULL");
+}
+
+int CountEven(PNODE head)
+{
+    int iCount = 0;
+
+    while(head != NULL)
+    {
+        if(head -> data % 2 == 0)
+        {
+            iCount++;
+        }
+        head = head -> next;
+    }
+    return iCount;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iCount = 0, i= 0,iNo = 0, iRet = 0;
+
+    printf("Enter the Number of Nodes: ");
+    scanf("%d",&iCount);
+
+    printf("Enter the Elements: ");
+    for(i = 1; i <= iCount; i++)
+    {
+        scanf("%d",&iNo);
+        InsertFirst(&head,iNo);
+    }
+
+    Display(head);
+
+    iRet = CountEven(head);
+
+    printf("Number of Even Elements: %d",iRet);
+
+    return 0;
+}
