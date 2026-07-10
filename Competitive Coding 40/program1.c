653464938aa651d8e7398a8a7db5557b51578a64
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-10 10:29:29 +0530

    Add C program to display odd elements in singly linked list   Defined NODE structure and typedefs for node pointers.   Implemented InsertFirst to add nodes at the beginning of the list.   Added Display function to traverse and print all nodes.   Created DisplayOdd function to show only odd elements from the list.   Integrated user input in main to build list, display all nodes, and print odd elements separately.

---
+++ Competitive Coding 40/program1.c
@@ -0,0 +1,77 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertFirst(PPNODE first, int No)
+{
+    PNODE newn = NULL;
+    
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = No;
+    newn -> next = NULL;
+
+    if(NULL == *first)
+    {
+        *first = newn;
+    }
+    else
+    {
+        newn -> next = *first;
+        *first = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("|%d| -> ",first -> data);
+        first = first -> next;
+    }
+    printf("NULL");
+}
+
+void DisplayOdd(PNODE head)
+{
+    while(head != NULL)
+    {
+        if(head -> data % 2 != 0)
+        {
+            printf("%d ",head -> data);
+        }
+        head = head -> next;
+    }
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iCount = 0, i= 0,iNo = 0, iRet = 0, iValue = 0;
+
+    printf("Enter the Number of Nodes: ");
+    scanf("%d",&iCount);
+
+    printf("Enter the Elements: ");
+    for(i = 1; i <= iCount; i++)
+    {
+        scanf("%d",&iNo);
+        InsertFirst(&head,iNo);
+    }
+
+    Display(head);
+
+    printf("\nOdd Elements are: ");
+    DisplayOdd(head);
+
+    return 0;
+}
