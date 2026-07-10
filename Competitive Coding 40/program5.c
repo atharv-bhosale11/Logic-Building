611d9b6dd7ab93793f28a59c38b802cd6db0c08a
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-10 11:01:33 +0530

    Add C program to count elements less than given number in singly linked list   Defined NODE structure and typedefs for node pointers.   Implemented InsertLast to add nodes at the end of the list.   Added Display function to traverse and print all nodes.   Created CountLess function to calculate number of nodes with data less than user input.   Integrated user input in main to build list, display elements, and show count of lesser numbers.

---
+++ Competitive Coding 40/program5.c
@@ -0,0 +1,88 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+typedef struct node NODE;
+typedef struct node *PNODE;
+typedef struct node **PPNODE;
+
+void InsertLast(PPNODE first, int No)
+{
+    PNODE newn = NULL;
+    PNODE temp = NULL;
+    
+    newn = (PNODE)malloc(sizeof(NODE));
+
+    newn -> data = No;
+    newn -> next = NULL;
+
+    if(NULL == *first)
+    {
+        *first = newn;
+    }
+    else
+    {
+        temp = *first;
+
+        while(temp -> next != NULL)
+        {
+            temp = temp -> next;
+        }
+        temp -> next = newn;
+    }
+}
+
+void Display(PNODE first)
+{
+    while(first != NULL)
+    {
+        printf("|%d| -> ",first -> data);
+        first = first -> next;
+    }
+    printf("NULL");
+}
+
+int CountLess(PNODE head,int No)
+{
+    int iCount = 0;
+    while(head != NULL)
+    {
+        if(head -> data < No)
+        {
+            iCount++;
+        }
+        head = head -> next;
+    }
+    return iCount;
+}
+
+int main()
+{
+    PNODE head = NULL;
+    int iCount = 0, i= 0,iNo = 0, iValue = 0;
+    int iRet = 0;
+
+    printf("Enter the Number of Nodes: ");
+    scanf("%d",&iCount);
+
+    printf("Enter the Elements: ");
+    for(i = 1; i <= iCount; i++)
+    {
+        scanf("%d",&iNo);
+        InsertLast(&head,iNo);
+    }
+
+    Display(head);
+    printf("\nEnter Number you have to find greater: ");
+    scanf("%d",&iValue);
+
+    iRet = CountLess(head,iValue);
+    printf("There are %d numbers are Lesser than %d",iRet,iValue);
+   
+    return 0;
+}
