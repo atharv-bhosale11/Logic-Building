3a1074954f928dc14436dbb8527002a10f28788a
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-22 12:29:50 +0530

    Add C++ program to demonstrate generic template for frequency count   Defined template function Frequency with type parameter T.   Implemented loop to compare elements and increment count when matched.   Tested template with int array {10,20,30,10,30,40,10,40,10} for element 10.   Displayed result to showcase reusability of template across different data types.

---
+++ Competitive Coding 42/program2.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+template <class T>
+
+int Frequency(T *arr, int iSize, T iNo)
+{
+    int i = 0, iCount = 0;
+
+    for(i = 0 ; i < iSize ; i++)
+    {
+        if(iNo == arr[i])
+        {
+            iCount++;
+        }
+    }
+    return iCount;
+}
+
+int main()
+{
+    int arr[] = {10,20,30,10,30,40,10,40,10};
+    int iRet = Frequency(arr,9,10);
+    cout<<iRet;
+    return 0;
+}
