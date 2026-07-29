e864dc5110ed9c089921b4fc2c6a24cf584160d0
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-29 20:54:50 +0530

    Add C program to count white spaces in file   Included fcntl, unistd, and string.h for system calls.   Defined CountWhite to open file in read-only mode.   Implemented buffer-based reading loop until EOF.   Checked for space character ' ' and incremented counter.   Handled error case for file open failure with return code.   Closed file descriptor after processing and returned count.   Demonstrated usage in main with user input filename and output.

---
+++ Competitive Coding 47/program3.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+#define BUFFER_SIZE 1024
+#define ERR_OPEN -1
+
+int CountWhite(char Fname[])
+{
+    char Buffer[BUFFER_SIZE] = {'\0'};
+    int iRet = 0, fd = 0, iCount = 0, i = 0;
+
+    fd = open(Fname, O_RDONLY);
+    
+    if(fd == -1)
+    {
+        return ERR_OPEN;
+    }
+
+    while((iRet = read(fd, Buffer, sizeof(Buffer))) != 0)
+    {
+        for(i = 0; i < iRet; i++)
+        {
+            if(Buffer[i] == ' ')
+            {
+                iCount++;
+            }
+        }
+        memset(Buffer, '\0', sizeof(Buffer));
+    }
+    close(fd);
+    return iCount;
+}
+
+int main()
+{
+    char FileName[20];  
+    int iRet = 0;
+
+    printf("Enter File Name: ");
+    scanf("%[^'\n']s",FileName);
+
+    iRet = CountWhite(FileName);
+
+    printf("Number of White Spaces: %d",iRet);
+
+    return 0;
+}
