c364b9be3baa25521fe9e3ccf3b6a9c841b4e787
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-07-30 20:59:21 +0530

    Add C program to create file using open system call   Included fcntl, unistd, and string.h for system calls.   Defined DisplayN to attempt file creation with O_CREAT flag.   Printed success message when file is created successfully.   Handled error case gracefully when file creation fails.   Closed file descriptor after successful creation.   Demonstrated usage in main with user input filename.

---
+++ Competitive Coding 48/program2.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<string.h>
+
+#define BUFFER_SIZE 1024
+
+void DisplayN(char Fname[])
+{
+    int fd = 0;
+
+    fd = open(Fname, O_CREAT,0777);
+
+    if(fd == -1)
+    {
+        return;
+    }
+    else
+    {
+        printf("File Create Succesfully!!");
+    }
+    close(fd);
+}
+
+int main()
+{
+    char FileName[20];
+
+    printf("Enter File Name: ");
+    scanf("%[^'\n']s",FileName);
+
+    DisplayN(FileName);
+
+    return 0;
+}
