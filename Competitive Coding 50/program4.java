b5441e83accf23b4d647842090203739690cbab5
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-08 19:18:46 +0530

    Display all files and their sizes from a directory

---
+++ Competitive Coding 50/program4.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class program4
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Directory Name : ");
+        String Dname = sobj.nextLine();
+
+        File fobj = new File(Dname);
+
+        if(fobj.exists() && fobj.isDirectory())
+        {
+            File Arr[] = fobj.listFiles();
+
+            System.out.println("Files in Directory : ");
+
+            for(int i = 0; i < Arr.length; i++)
+            {
+                if(Arr[i].isFile())
+                {
+                    System.out.println(
+                        Arr[i].getName() +
+                        " -> " +
+                        Arr[i].length() +
+                        " bytes"
+                    );
+                }
+            }
+        }
+        else
+        {
+            System.out.println("Directory not found");
+        }
+    }
+}
