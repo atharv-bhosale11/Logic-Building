aa5159b1c73a27c57ca81f91cc9c9e9a87bfed75
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-08 19:17:55 +0530

    Create a new directory using File class

---
+++ Competitive Coding 50/program2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+class program2
+{
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter Directory Name : ");
+        String Dname = sobj.nextLine();
+
+        File fobj = new File(Dname);
+
+        if(fobj.exists())
+        {
+            System.out.println("Directory already exists");
+        }
+        else
+        {
+            fobj.mkdir();
+            System.out.println("Directory created successfully");
+        }
+    }
+}
