3d6462574f3f84083c93d3e23e265b4882480868
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-08 19:14:31 +0530

    Added Java program to count and list files in a folder

---
+++ Competitive Coding 49/program5.java
@@ -0,0 +1,37 @@
+import java.io.*;      
+import java.util.*;    
+
+class program688
+{
+    public static void main(String A[]) throws IOException
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        String FolderName = null;
+
+        System.out.println("Enter Folder Name: ");
+        FolderName = sobj.nextLine();
+
+        File fobj = new File(FolderName);
+
+        if((fobj.exists()) && (fobj.isDirectory()))
+        {
+            System.out.println("Folder exists");
+
+            File fArr[] = fobj.listFiles();
+
+            System.out.println("Number of Files in Folder : " + fArr.length);
+
+            for(int i = 0; i < fArr.length; i++)
+            {
+                System.out.println(fArr[i].getName());
+            }
+        }
+        else
+        {
+            System.out.println("There is no such folder");
+        }
+
+        sobj.close();
+    }
+}
