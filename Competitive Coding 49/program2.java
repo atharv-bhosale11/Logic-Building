80037289ecb65e5e6b97fea01197af06d2bffd0c
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-08 19:05:40 +0530

    Read and display file contents using FileInputStream

---
+++ Competitive Coding 49/program2.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.*;
+
+class program683
+{
+    public static void main(String A[]) throws IOException
+    {
+        String Fname = null;
+        int iRet = 0;
+        File fobj = null;
+        FileInputStream fiobj = null;
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter File Name: ");
+      
+        Fname = sobj.nextLine();
+
+        fobj = new File(Fname);
+
+        fiobj = new FileInputStream(fobj);
+ 
+        byte Buffer[] =  new byte[50];
+        
+        if(fobj.exists())
+        {
+            String str = null;
+
+            while((iRet = fiobj.read(Buffer)) != -1)
+            {
+                str = new String(Buffer,0,iRet);        // Important
+
+                System.out.print(str);
+                str = null;
+            }
+        }
+        else
+        {
+            System.out.println("There is no Such File");
+        }
+    }
+}
