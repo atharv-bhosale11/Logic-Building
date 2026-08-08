b8e98beae9e7001a32aca70e05d24db5f3306581
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-08 19:09:07 +0530

    Added Java program to create file using File class

---
+++ Competitive Coding 49/program3.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+class first
+{
+    public static void main(String A[])
+    {
+        String Fname = null;
+
+        Scanner sobj = new Scanner(System.in);
+
+        try
+        {
+
+            System.out.println("Enter file name : ");
+            Fname = sobj.nextLine();
+    
+            File fobj = new File(Fname);
+
+            if(fobj.exists())
+            {
+                System.out.println("File already exists");
+            }
+            else
+            {
+                // Create new file
+                fobj.createNewFile();
+
+                System.out.println("File created successfully");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println("Unable to create file");
+        }
+
+        sobj.close();
+    }
+}
