3f60b8653037ccc286b06d8fc3677fd6f79ae1ef
Atharv Bhosale <atharv.t.bhosale112@gmail.com>
2026-08-18 20:41:56 +0530

    Add Java program to print duplicate characters from a string   Implemented FindDuplicates method to iterate through characters, counted occurrences, checked for duplicates, avoided reprinting already processed characters, displayed duplicates in one line, and handled case when no duplicates are found

---
+++ Competitive Coding 60/program4.java
@@ -0,0 +1,76 @@
+/*
+Q4. Write a program to print duplicate characters from a string.
+Description:
+Find and display characters that appear more than once.
+
+Input Format: One string str
+Output Format: Duplicate characters printed in one line.
+*/
+
+import java.util.*;
+
+class program4
+{
+    public static void FindDuplicates(String Str)
+    {
+        int iLen = Str.length();
+        int i    = 0;
+        int j    = 0;
+        boolean bFound = false;
+
+        for(i = 0; i < iLen; i++)
+        {
+            int iCount = 0;
+
+            for(j = 0; j < iLen; j++)
+            {
+                if(Str.charAt(i) == Str.charAt(j))
+                {
+                    iCount++;
+                }
+            }
+
+           
+            if(iCount > 1)
+            {
+                
+                boolean bAlreadyPrinted = false;
+
+                for(j = 0; j < i; j++)
+                {
+                    if(Str.charAt(i) == Str.charAt(j))
+                    {
+                        bAlreadyPrinted = true;
+                        break;
+                    }
+                }
+
+                if(bAlreadyPrinted == false)
+                {
+                    System.out.print(Str.charAt(i) + " ");
+                    bFound = true;
+                }
+            }
+        }
+
+        if(bFound == false)
+        {
+            System.out.println("No duplicate characters found");
+        }
+        else
+        {
+            System.out.println();
+        }
+    }
+
+    public static void main(String A[])
+    {
+        Scanner sobj = new Scanner(System.in);
+        String Str   = null;
+
+        System.out.println("Enter string :");
+        Str = sobj.nextLine();
+
+        FindDuplicates(Str);
+    }
+}
